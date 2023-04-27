d57bcb535b61958707207f9264c6230218ca77ad
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2023-04-27 12:33:50 +0545

    06.py

---
+++ 06.py
@@ -0,0 +1,6 @@
+# multiplication table of a given number
+
+inputNumber = int(input("Enter a no:"))
+rangeOfMultiply = int(input("Enter a range"))
+for i in range(1, rangeOfMultiply+1):
+    print(f'{inputNumber}*{i}={inputNumber*i}')
