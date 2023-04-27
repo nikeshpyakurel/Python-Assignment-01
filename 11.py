15dcb72e3bf693997b75616556c3c9d918b1424f
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2023-04-27 13:27:20 +0545

    11.py

---
+++ 11.py
@@ -0,0 +1,17 @@
+# Python program that accepts a string and calculate the number of digits and letters and space
+
+inputString = input("Enter String:")
+calculateSpace = 0
+calculateDigit = 0
+calculateLetter = 0
+
+
+for i in inputString:
+    if i.isdigit():
+        calculateDigit += 1
+    elif i.isspace():
+        calculateSpace += 1
+    elif i.isalpha():
+        calculateLetter += 1
+print(
+    f'totoal letter={calculateLetter},total space-{calculateSpace},total digit={calculateDigit}')
