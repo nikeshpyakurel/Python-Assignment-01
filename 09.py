6f15ad1774107d27dcdf7dc196b7067036f16957
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2023-04-27 13:07:18 +0545

    prime no

---
+++ 09.py
@@ -0,0 +1,19 @@
+# given number is prime or not
+
+inputNumber = int(input("Enter Prime"))
+
+initialValue = 2
+if inputNumber == 1:
+    print("not prime")
+
+elif inputNumber == 2:
+    print("prime")
+else:
+    for i in range(initialValue, inputNumber, initialValue+1):
+        if inputNumber % i == 0:
+            print("Not Prime")
+            break
+        else:
+            print("Prime")
+            break
+        i
